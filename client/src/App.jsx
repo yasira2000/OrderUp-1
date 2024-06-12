f7c49369366f3d066c8eeaf2e23a52e8a09864f5
Dilshan Rakshitha <dilshanrakshitha2000@gmail.com>
2024-06-12 13:49:41 +0530

    installed required packages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import {BrowserRouter ,Routes, Route} from 'react-router-dom'
+import Header from '../components/Header.jsx'
+import Home from './Home.jsx'
 
 export default function App() {
   return (
-    <div>app</div>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
